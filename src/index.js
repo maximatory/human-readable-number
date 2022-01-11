977aaf6c62f8ba739ceff7076c9523cd3b815450
Maksim Baranov <oooinfo@mail.ru>
2022-01-11 16:43:25 +0300

    fix: fix code

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
 module.exports = function toReadable(number) {
+   number = number.toString()
    let one = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
    let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-   let hundreds = ['','one hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred'];
+   let hundreds = ['', 'one hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred'];
 
-   let sotni = Math.floor(number/100);
-   let desatki = Math.floor((number / 10) % 10);
-   let edinici = Math.floor(number % 10);
-
-   if (number <= 19) {
-      return(one[number]);
-   } else if (number >= 20 && number < 100) {
-      return(`${tens[desatki]}-${one[edinici]}`)
-   } else if (number >= 100 && number <= 999) {
-      if (desatki === 0) {
-         return(`${hundreds[sotni]} ${one[edinici]}`);
-      } else if (desatki !== 0 && edinici !== 0) {
-         return(`${hundreds[sotni]} ${tens[desatki]} ${one[edinici]}`);
-      } else if (edinici === 0) {
-         return(`${hundreds[sotni]} ${tens[desatki]}`);
-       }
-   }
+   if (number.length == 1 || number>9&&number<20) {
+      return one[number];
+    } else if (number>20&&number<=99&&number[1]!=0){
+      return tens[number[0]]+" "+one[number[1]];
+    } else if (number>=20 && number<= 99 && number[1] == 0) {
+      return tens[number[0]]
+    } else if (number.length == 3 && Number(number[1]) + Number(number[2])==0) {
+      return hundreds[number[0]]
+    } else if (number > 100 && (number[1] + number[2] > 9 && number[1] + number[2]<20)) {
+      return hundreds[number[0]] + " " + one[number[1] + number[2]]
+    } else if (number > 100 && (number - 100 * number[0]) <20) {
+      return hundreds[number[0]] + " " + one[number - 100 * number[0]]
+    } else if (number > 100 && number[2]!=0){
+      return hundreds[number[0]] + " " + tens[number[1]] + " " + one[number[2]];
+    } else {
+      return hundreds[number[0]] + " " + tens[number[1]]
+    }
 }
+
 
 
 
